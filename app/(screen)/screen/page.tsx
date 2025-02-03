6e26eb51c2776d01d2ce46bdb0585953163af0c3
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2025-02-03 17:08:46 +0700

    feat: next queue

--- app/(screen)/screen/page.tsx
+++ app/(screen)/screen/page.tsx
@@ -31,22 +31,29 @@ export default function Screen() {
 
   const { data, mutate } = useSWR<any>(
     `${process.env.NEXT_PUBLIC_API_URL}/user/instansi/screen`,
-    fetcher,
+    fetcher
   );
+
+  const { data: video } = useSWR<any>(
+    `${process.env.NEXT_PUBLIC_API_URL}/user/video/get`,
+    fetcher
+  );
+
+  const resultVideo = video?.data?.video;
 
   useEffect(() => {
     // Periksa apakah data sudah tersedia dan set state result dan antrianLast
     if (data && data.data) {
-      setCount(data?.data?.count || 0)
+      setCount(data?.data?.count || 0);
       setResult(data.data.antrian || []);
       setAntrianLast(data.data.antrian_last || null);
     }
 
     socket.on("updateAntrian", (data: any) => {
       // Memanggil ulang mutate untuk memperbarui data
-     
+
       if (data == instansiid) {
-        console.log("apaaa", data)
+        console.log("apaaa", data);
         mutate();
       }
     });
@@ -84,18 +91,18 @@ export default function Screen() {
 
       // Format hari, tanggal, bulan, tahun
       const options: Intl.DateTimeFormatOptions = {
-        weekday: 'long',
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
       };
-      const formattedDate = now.toLocaleDateString('id-ID', options);
+      const formattedDate = now.toLocaleDateString("id-ID", options);
 
       // Format jam dan menit
-      const formattedTime = now.toLocaleTimeString('id-ID', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit', // Tambahkan detik untuk memperbarui setiap detik
+      const formattedTime = now.toLocaleTimeString("id-ID", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit", // Tambahkan detik untuk memperbarui setiap detik
         hour12: false,
       });
 
@@ -112,43 +119,47 @@ export default function Screen() {
 
   const settings = {
     infinite: true,
-    slidesToShow: count > 4 ? 4 : count,
+    slidesToShow: count > 2 ? 2 : count,
     slidesToScroll: 1,
-    autoplay: count > 4 ? true : false,
-    autoplaySpeed: 5000,
+    autoplay: count > 2 ? true : false,
+    autoplaySpeed: 3000,
     cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      }
-    ]
+    arrows: false, // Hilangkan tombol navigasi kiri-kanan
+    dots: false,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
   };
 
   return (
-    <ProtectedRoute roles={["Admin Instansi", "Admin Layanan", "Admin Verifikasi"]}>
-      <div className="flex flex-col w-full px-24 gap-y-4">
+    <ProtectedRoute
+      roles={["Admin Instansi", "Admin Layanan", "Admin Verifikasi"]}
+    >
+      <div className="flex flex-col w-full px-24 gap-y-4 h-full">
         <div className="flex flex-row w-full gap-x-4 mt-3">
           <div className="flex flex-row w-1/12">
             <Image
-              src="/images/DesignLogoMpp.svg"
+              src="/logo-1.png"
               alt="Lampung Timur"
               className="w-full h-full object-fill"
               width={1000}
@@ -164,13 +175,19 @@ export default function Screen() {
 
           <div className="grid grid-cols-2 place-items-center justify-center items-center bg-secondary-700 rounded-lg w-4/12">
             <div className="flex flex-col">
-              <h3 className="text-neutral-50 text-[20px]">{tanggal.split(',')[0]}</h3>
+              <h3 className="text-neutral-50 text-[20px]">
+                {tanggal.split(",")[0]}
+              </h3>
 
-              <p className="text-neutral-50 text-[20px]">{tanggal.split(',')[1]}</p>
+              <p className="text-neutral-50 text-[20px]">
+                {tanggal.split(",")[1]}
+              </p>
             </div>
 
             <div className="">
-              <h3 className="text-neutral-50 text-[32px] font-semibold">{jam}</h3>
+              <h3 className="text-neutral-50 text-[32px] font-semibold">
+                {jam}
+              </h3>
             </div>
           </div>
         </div>
@@ -181,48 +198,90 @@ export default function Screen() {
               Nomor Antrian
             </h4>
 
-            <h2 className="text-neutral-50 font-semibold text-[50px]">{antrianLast?.antrian_now}</h2>
+            <h2 className="text-neutral-50 font-semibold text-[50px]">
+              {antrianLast?.antrian_now}
+            </h2>
 
-            <h5 className="text-neutral-50 font-normal text-[25px]">Loket {antrianLast?.code}</h5>
+            <h5 className="text-neutral-50 font-normal text-[25px]">
+              Loket {antrianLast?.code}
+            </h5>
           </div>
 
           <div className="bg-neutral-50 w-full rounded-lg">
             <div className="flex flex-row w-full">
-              <Image
-                src="/images/yt.png"
-                alt="Youtube"
-                className="w-full h-full object-fit rounded-lg"
-                width={1000}
-                height={500}
-              />
+              <video controls autoPlay loop className="w-full h-[480px]">
+                <source src={resultVideo} type="video/mp4" />
+                Browser Anda tidak pemutaran video.
+              </video>
             </div>
           </div>
         </div>
 
-        <div className="slider-container">
-          <Slider {...settings}>
-            {result?.map((item: any, index: any) => (
-              <div
-                key={index}
-                className={`rounded-2xl p-8 ${index % 4 === 0
-                  ? "bg-[#3597FC]"
-                  : index % 4 === 1
-                    ? "bg-[#00CC83]"
-                    : index % 4 === 2
-                      ? "bg-[#ED4B54]"
-                      : "bg-[#A636FF]"
-                  }`}
-              >
-                <h4 className="text-neutral-50 font-semibold text-[46px]">
-                  {item.antrian_now}
-                </h4>
+        <div className="flex space-x-8">
+          <div className="w-1/2">
+            <p className="capitalize text-slate-800 text-[24px] font-semibold">
+              Antrean Saat Ini
+            </p>
 
-                <h5 className="text-neutral-50 font-normal text-[26px]">
-                  {`Loket ${item.code}`}
-                </h5>
-              </div>
-            ))}
-          </Slider>
+            <div className="slider-container mt-2">
+              <Slider {...settings}>
+                {result?.map((item: any, index: any) => (
+                  <div
+                    key={index}
+                    className={`rounded-2xl p-8 mr-4 ${
+                      index % 4 === 0
+                        ? "bg-[#3597FC]"
+                        : index % 4 === 1
+                          ? "bg-[#00CC83]"
+                          : index % 4 === 2
+                            ? "bg-[#ED4B54]"
+                            : "bg-[#A636FF]"
+                    }`}
+                  >
+                    <h4 className="text-neutral-50 font-semibold text-[46px]">
+                      {item.antrian_now}
+                    </h4>
+
+                    <h5 className="text-neutral-50 font-normal text-[26px]">
+                      {`Loket ${item.code}`}
+                    </h5>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <p className="capitalize text-slate-800 text-[24px] font-semibold">
+              Antrean Selanjutnya
+            </p>
+
+            <div className="slider-container mt-2">
+              <Slider {...settings}>
+                {result?.map((item: any, index: any) => (
+                  <div
+                    key={index}
+                    className={`rounded-2xl p-8 mr-4 ${
+                      index % 4 === 0
+                        ? "bg-[#c815ff]"
+                        : index % 4 === 1
+                          ? "bg-[#ff7417]"
+                          : index % 4 === 2
+                            ? "bg-[#55090d]"
+                            : "bg-[#16d0ff]"
+                    }`}
+                  >
+                    <h4 className="text-neutral-50 font-semibold text-[46px]">
+                      {item.antrian_next}
+                    </h4>
+
+                    <h5 className="text-neutral-50 font-normal text-[26px]">
+                      {`Loket ${item.code}`}
+                    </h5>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
         </div>
       </div>
     </ProtectedRoute>
